e3c476f83c5ce3a9158f82a05612b6217c94566c
Junseop-Shin <nuclearbomb6518@gmail.com>
2025-04-07 18:00:14 +0900

    [bugfix] section에 패딩추가 - 패딩 추가

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -9,7 +9,7 @@ const Section = ({ children, ...props }: SectionProps) => {
   return (
     <FadeUpAnimation>
       <section
-        className="flex flex-col items-center justify-center h-screen text-center"
+        className="flex flex-col items-center justify-center h-screen text-center p-8"
         {...props}
       >
         {children}
