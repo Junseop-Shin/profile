27b353cad5ca8cd16d9911fef0450b50214d54e7
Junseop-Shin <nuclearbomb6518@gmail.com>
2025-04-09 20:35:39 +0900

    [feat] ProjectDetail에 이미지 추가 - Sticky 이미지 추가, 스크롤하면 opacity 적용 - 글자 opacity 적용 및 다크모드 색상 추가

--- src/components/ProjectDetail.tsx
+++ src/components/ProjectDetail.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Section from "./common/Section";
 import { useParams } from "react-router-dom";
 import { Project, projects } from "../assets/projects";
@@ -12,6 +12,24 @@ import H4 from "./common/H4";
 const ProjectDetail: React.FC = () => {
   const { projectId } = useParams<{ projectId: string }>();
   const project = projects.find((p) => p.link === projectId);
+
+  const [imageOpacity, setImageOpacity] = useState(1);
+
+  // Scroll event handler
+  const handleScroll = () => {
+    const scrollTop = window.scrollY; // Current scroll position
+    const maxScroll = window.innerHeight; // Maximum scroll height (adjust as needed)
+    const newOpacity = Math.max(0.2, 1 - scrollTop / maxScroll); // Calculate opacity (minimum 0.5)
+    setImageOpacity(newOpacity);
+  };
+
+  // Attach scroll listener
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll); // Cleanup listener
+    };
+  }, []);
 
   if (!project) {
     return <H2>프로젝트를 찾을 수 없습니다.</H2>;
@@ -41,7 +59,7 @@ const ProjectDetail: React.FC = () => {
     return (
       <div
         key={project.link}
-        className="flex flex-col w-full min-h-[50dvh] text-left p-6 shadow-lg rounded-lg"
+        className="flex flex-col w-full min-h-[50dvh] text-left p-6 shadow-lg rounded-lg dark:bg-gray-600"
       >
         <H3>{`${project.subtitle} (${project.date})`}</H3>
         <H4>{project.skills}</H4>
@@ -55,35 +73,54 @@ const ProjectDetail: React.FC = () => {
   const { subProjects } = project;
   return (
     <Section id="project-detail">
-      <H2>{project.title}</H2>
-      <div className="flex flex-col gap-3 text-left mb-6">
-        {project.web && (
-          <a
-            href={project.web}
-            className="flex items-center gap-1 hover-target"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon iconName={IoMdAt} />
-            <Span>{project.web}</Span>
-          </a>
-        )}
-        {project.git && (
-          <a
-            href={project.git}
-            className="flex items-center gap-1 hover-target"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon iconName={IoLogoGithub} />
-            <Span>{project.git}</Span>
-          </a>
-        )}
-      </div>
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8">
-        {subProjects
-          ? subProjects.map((subProject) => layoutProject(subProject))
-          : layoutProject(project)}
+      <img
+        src={project.image}
+        alt={project.title}
+        className="w-full object-contain opacity-90 my-2 sticky top-0 transition-opacity duration-100"
+        style={{ opacity: imageOpacity }}
+      />
+      <div
+        className={`w-full h-full flex flex-col items-center p-6 ${
+          imageOpacity !== 1 ? "bg-white dark:bg-gray-400" : "bg-none"
+        }`}
+        style={{
+          opacity: 1 - imageOpacity,
+        }}
+      >
+        <H2>{project.title}</H2>
+        <div className="flex flex-col gap-3 text-left mb-6">
+          {project.web && (
+            <a
+              href={project.web}
+              className="flex items-center gap-1 hover-target"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon iconName={IoMdAt} />
+              <Span>{project.web}</Span>
+            </a>
+          )}
+          {project.git && (
+            <a
+              href={project.git}
+              className="flex items-center gap-1 hover-target"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon iconName={IoLogoGithub} />
+              <Span>{project.git}</Span>
+            </a>
+          )}
+        </div>
+        <div
+          className={`${
+            subProjects ? "sm:grid-cols-1 md:grid-cols-2" : "grid-cols-1"
+          } grid gap-8`}
+        >
+          {subProjects
+            ? subProjects.map((subProject) => layoutProject(subProject))
+            : layoutProject(project)}
+        </div>
       </div>
     </Section>
   );
